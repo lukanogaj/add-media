b9d0bccfda6b58178145923b523c3d46f960eab1
Lukasz Nogaj <lukasznogaj@Lukaszs-Mac-mini.local>
2023-11-01 12:24:27 +0000

    cross-button

---
+++ src/components/MediaBtn/index.jsx
@@ -0,0 +1,11 @@
+import styles from './index.module.scss';
+
+const MediaBtn = () => {
+  return (
+    <div className={styles.hero}>
+      <button className={styles.btn}></button>
+    </div>
+  );
+};
+
+export default MediaBtn;
